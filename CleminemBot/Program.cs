ac691022ea0b5be6d6a41175a4b0db660ef21729
Clément Sciallano <csciallano@users.noreply.github.com>
2022-04-21 16:37:09 +0200

    Add files via upload

---
+++ CleminemBot/Program.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// The wonderful Cleminem bot program
+/// </summary>
+public class Program
+{
+
+    // The main method starts our bot
+    static void Main(string[] args)
+    {
+        new CleminemBot().Start();
+    }
+}
